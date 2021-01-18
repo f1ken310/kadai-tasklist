64d0f0fa9d6e1406c3b37f31c82d9d71c1d5bda0
takuma.sato <tkm310@gmail.com>
2021-01-18 13:50:04 +0000

    add status

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,11 @@
 class TasksController < ApplicationController
+    before_action :set_message, only: [:show, :edit, :update, :destroy]
+ 
     def index
         @tasks = Task.all
     end
     
     def show
-        @task = Task.find(params[:id])
     end
     
     def new
@@ -12,6 +13,7 @@ class TasksController < ApplicationController
     end
     
     def create
+        
         @task = Task.new(task_params)
 
         if @task.save
@@ -24,11 +26,9 @@ class TasksController < ApplicationController
     end
     
     def edit
-        @task = Task.find(params[:id])
     end
     
     def update
-        @task = Task.find(params[:id])
 
         if @task.update(task_params)
             flash[:success] = 'Task は正常に更新されました'
@@ -40,16 +40,20 @@ class TasksController < ApplicationController
     end
     
     def destroy
-        @task = Task.find(params[:id])
         
         @task.destroy
 
         flash[:success] = 'Task は正常に削除されました'
         redirect_to tasks_url
     end
+    
+    def set_message
+    @task = Task.find(params[:id])
+    end
+    
 
     def task_params
-        params.require(:task).permit(:content)
+        params.require(:task).permit(:content, :status)
     end
 
 end  
