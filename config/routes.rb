364ef1f680146b0b67fc72f586840e44991608e1
takuma.sato <tkm310@gmail.com>
2021-01-27 14:21:26 +0000

    top_page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
+  get 'users/show'
+  get 'users/new'
+  get 'users/create'
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
     root to: 'tasks#index'
+    
+    get 'login', to: 'sessions#new'
+    post 'login', to: 'sessions#create'
+    delete 'login', to: 'sessions#destroy'
+    
+    get 'signup', to: 'users#new'
     
     resources :tasks
     
