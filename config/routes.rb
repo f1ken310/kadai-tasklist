6d80a6e0ef4a4019093b21bdd36f820411ebb697
takuma.sato <tkm310@gmail.com>
2021-01-29 13:23:52 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,13 @@
 Rails.application.routes.draw do
   
-  get 'users/show'
-  get 'users/new'
-  get 'users/create'
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
     root to: 'tasks#index'
     
     get 'login', to: 'sessions#new'
     post 'login', to: 'sessions#create'
-    delete 'login', to: 'sessions#destroy'
+    delete 'logout', to: 'sessions#destroy'
     
     get 'signup', to: 'users#new'
     
     resources :tasks
-    resources :users, only:[:create, :new]
-    
+    resources :users, only:[:create, :new, :show]
 end
