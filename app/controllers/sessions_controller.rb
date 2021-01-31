20d732384fcbf4e2c2fc9aea7f98be9515393b1f
takuma.sato <tkm310@gmail.com>
2021-01-31 15:48:09 +0000

    delete_extra

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
   
   def new
-  end
+  end  
   
   def create
     email = params[:session][:email].downcase
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
     before_action :require_user_logged_in
-    before_action :correct_user, only:  [:crate, :update, :destroy]
+    before_action :correct_user, only:  [:show, :crate, :edit, :update, :destroy]
  
     def index
       if logged_in?
@@ -10,7 +10,6 @@ class TasksController < ApplicationController
     end
     
     def show
-        @task = Task.find(params[:id])
     end
     
     def new
@@ -31,12 +30,10 @@ class TasksController < ApplicationController
     end
     
     def edit
-         @task = Task.find(params[:id])
     end
     
     def update
-         @task = Task.find(params[:id])
-
+        
         if @task.update(task_params)
             flash[:success] = 'Task は正常に更新されました'
             redirect_to root_url
