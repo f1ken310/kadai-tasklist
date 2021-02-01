f1ba192fc38ef220003d7d8e596952b2b6e53c9e
takuma.sato <tkm310@gmail.com>
2021-02-01 13:57:52 +0000

    edit_task_controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,8 @@
 class TasksController < ApplicationController
     before_action :require_user_logged_in
-    before_action :correct_user, only:  [:show, :crate, :edit, :update, :destroy]
+    before_action :correct_user, only:  [:show, :crate, :edit, :update]
  
     def index
-      @task = current_user.tasks.build  
       @tasks = current_user.tasks.order(id: :desc).page(params[:page])
     end
     
